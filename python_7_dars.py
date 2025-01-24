ad63c203e5f3aa805db0916ba4364726da0d7fd1
Asliddincoder0504 <168742217+Asliddincoder0504@users.noreply.github.com>
2025-01-24 18:49:55 +0500

    darslar
    darslar

---
+++ python_7_dars.py
@@ -0,0 +1,249 @@
+#String  metodlari
+
+
+###capitalze
+# matn1="assalomu alaykum yaxshimisizlar"
+# a=matn1.capitalize()
+# print(a)
+
+
+###casefold
+# matn2="Salom  Hammaga Darslar  Bo'ldimi"
+# a=matn2.casefold()
+# print(a)
+
+
+###center
+# matn3="Asliddin"
+# a=matn3.center(24)
+# print(a)
+
+
+###count
+# matn4="Men albatta Backend dasturchi  bo'laman"
+# a=matn4.count("Backend")
+# print(a)
+
+
+###encode
+# matn5="Meni  Öğrenci "
+# a=matn5.encode()
+# print(a)
+
+
+###endswith
+# matn6="Salom  yaxshimisizlar  kursdoshlar."
+# a=matn6.endswith(".")
+# print(a)
+
+
+###expandtabs
+# matn7="S\ta\tl\to\tm"
+# a=matn7.expandtabs(4)
+# print(a)
+
+
+###find
+# matn8="Python  boshqa dasturlash tillaridan  mashhurroq"
+# a=matn8.find("dasturlash")
+# print(a)
+
+
+###format
+# matn9="Bu mashina  narxi {price:.2f} dollar"
+# a=matn9.format(price=2000)
+# print(a)
+
+
+###index
+# matn10="Salom, backend dasturchilar"
+# a=matn10.index("backend")
+# print(a)
+
+
+###isalnum
+# matn11="Asliddin0504"
+# a=matn11.isalnum()
+# print(a)
+
+
+###isalpha
+# matn12="Dasturchilar"
+# a=matn12.isalpha()
+# print(a)
+
+
+###isascii
+# matn13="Cosder1512"
+# a=matn13.isascii()
+# print(a)
+
+
+###isdecimal
+# matn14="1234324"
+# a=matn14.isdecimal()
+# print(a)
+
+
+###isdigit
+# matn15="46534434"
+# a=matn15.isdigit()
+# print(a)
+
+
+###isidentifier
+# matn16="coder0504"
+# a=matn16.isidentifier()
+# print(a)
+
+
+###islower
+# matn17="  salom  hammaga  "
+# a=matn17.islower()
+# print(a)
+
+
+###isnumeric
+# matn18="34543"
+# a=matn18.isnumeric()
+# print(a)
+
+
+###isprintable
+# matn29="Mening  ismim  Asliddin"
+# a=matn29.isprintable()
+# print(a)
+
+
+###isspace
+# matn20="  "
+# a=matn20.isspace()
+# print(a)
+
+
+
+###istitle
+# matn21="Asliddin  Nasib  Inshaalloh  Dasturchi  Boladi"
+# a=matn21.istitle()
+# print(a)
+
+
+###isupper
+# matn22="MATEMATIKA  FIZIKA  DASTURLASH"
+# a=matn22.isupper()
+# print(a)
+
+
+###join
+# mytuple23=("Asliddin","Ozodbek","Javohir")
+# a="*".join(mytuple)
+# print(a)
+
+
+###ljust
+# matn24="men"
+# a=matn24.ljust(14)
+# print(a,"dasturlashni  organyabman")
+
+
+###lower
+# matn25="Mening  dostim  OZODBEK"
+# a=matn25.lower()
+# print(a)
+
+
+###lstrip
+# matn26="kitob"
+# a=matn26.lstrip()
+# print("o'qish zo'r  ayniqsa   badiiy ",a ,"larni")
+
+
+###maketrans
+# matn27="cam scaner"
+# a=matn27.maketrans("c","t")
+# print(matn27.translate(a))
+
+
+###partition
+# matn28="Menga  kitob  o'qish  yoqadi"
+# a=matn28.partition("kitob")
+# print(a)
+
+
+###replace
+# matn29="I  like  book"
+# a=matn29.replace("book","computer")
+# print(a)
+
+
+###rfind
+# matn30="Sen  mening yaqin  do'stimsan"
+# a=matn30.rfind("yaqin")
+# print(a)
+
+
+###rindex
+# matn31="Men Surxondaryo  viloyatida   yashayman"
+# a=matn31.rindex("viloyatida")
+# print(a)
+
+
+###rjust
+# matn32="xurmo"
+# a=matn32.rjust(20)
+# print(a, "mening  eng  yoqtirgan mevam")
+
+
+###rpartition
+# matn33="Men   kompyuter  oyinlariga qiziqaman  lekin  oynay  olmayman"
+# a=matn33.rpartition("qiziqaman")
+# print(a)
+
+
+###rsplit
+# matn34="BMW,Mers,Audi"
+# a=matn34.rsplit()
+# print(a)
+
+
+###splitlines
+# matn35="Olim  bo'lsang \n olam   seniki"
+# a=matn35.splitlines()
+# print(a)
+
+
+###startswith
+# matn36="    Mening  ismim Asliddin  "
+# a=matn36.startswith("Mening")
+# print(a)
+
+###strip
+# matn37="     cobalt      "
+# a=matn37.strip()
+# print("juda  ixcham ",a,"mashinasi")
+
+
+###title
+# matn38="salom  mening ismim asliddin"
+# a=matn38.title()
+# print(a)
+
+
+###translate
+# lugatim39 = {83:  80}
+# txt = "Hello Sam!"
+# print(txt.translate(lugatim))
+
+
+###upper
+# matn40="mening  do'stim  Jo'rabek"
+# a=matn40.upper()
+# print(a)
+
+
+###zfill
+# matn41= "50"
+#
+# x = matn41.zfill(7)
+#
+# print(x)
---
+++ python_8_dars.py
@@ -0,0 +1,140 @@
+#Append   metodi  list oxiriga   element qo'shadi
+# from collections import Counter
+# cars= ["BMW", "Lexus", "Bugatti", "Ferrari","BMW","Lexus","Cobalt","Malibu","BYD","Gentra"]
+# cars.sort()
+# print(cars)
+# a=Counter(cars)
+# print(a)
+
+
+
+#Clear  metodi   ro'yxat  elementlaarini  o'chirib  tozalab  tashlaydi
+# cars= ["BMW", "Lexus", "Bugatti", "Ferrari"]
+# cars.clear()
+# print(cars)
+
+
+#Copy   metodi   ro'yxatni  kopiya  qiladi
+# Ismlar=["Abdulloh","Muhammadyusuf","Ali","Asliddin"]
+# a=Ismlar.copy()
+# print(Ismlar)
+
+
+#Count  metodi  listda  sanalayotgan  elementdan  nechta  borligini  sanaydi
+# fruits=["apple","Orange","Cherry","Cherry"]
+# a=fruits.count("Cherry")
+# print(a)
+
+
+#Extend   metodi   birinchi  ro'yxat ichiga  ikkinchi  royxat  elementlarini  qo'shib  tashlaydi
+# product=["potato","carrot","onion"]
+# birds=["swallow","sparrow","dove"]
+# product.extend(birds)
+# print(product)
+
+
+#Index  metodi   kiritilgan  element  indeksini  chiqarib  beradi
+# animals=["cat","dog","cow","goat"]
+# print(animals.index("cow"))
+
+
+#Insert  metodi  royxatga  berilgan  indeks  boyicha  elelment qo'shadi
+# animals=["cat","dog","cow","goat"]
+# animals.insert(2,"sheep")
+# print(animals)
+
+
+#Pop   royxatning  elementini  berilgan   indeks  boyicha   olib  tashlaydi
+# product=["potato","carrot","onion","cabbage"]
+# product.pop(2)
+# print(product)
+
+
+#Remove   element nomini  bo'yicha royxatdan  elementni  o'chiradi
+# Ismlar=["Abdulloh","Muhammadyusuf","Ali","Asliddin"]
+# Ismlar.remove("Asliddin")
+# print(Ismlar)
+
+
+#Reverse  metodi  royxat  elementlarini  teskari  tartibda  chiqaradi
+# birds=["swallow","sparrow","dove"]
+# birds.reverse()
+# print(birds)
+
+#Sort  metodi   royxatni  sartarofka  qilib  beradi
+# sonlar=[23,76,345,34565,34,87,29]
+# sonlar.sort()
+# print(sonlar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
